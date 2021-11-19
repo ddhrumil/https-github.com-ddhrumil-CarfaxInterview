ecca6827daa0a4c6492aa79e0ca518fa3d607910
Dhrumil <dhrumildesai181@gmail.com>
2021-11-19 10:53:03 -0500

    Configured collection view to display data and added code

--- CarfaxInterview/CollectionViewCell.swift
+++ CarfaxInterview/CollectionViewCell.swift
@@ -1,38 +1,41 @@
 //
 //  CollectionViewCell.swift
-//  CarfaxInterview
+//  Carfax
 //
-//  Created by Dhrumil Desai on 2021-11-19.
+//  Created by Dhrumil Desai on 2021-11-18.
 //
 
 import UIKit
 
+
 class CollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var imageOutlet: UIImageView!
     @IBOutlet weak var yearOutlet: UILabel!
-    
     
     @IBOutlet weak var makeOutlet: UILabel!
     
     @IBOutlet weak var modelOutlet: UILabel!
     
-    
     @IBOutlet weak var trimOutlet: UILabel!
-    
     @IBOutlet weak var priceOutlet: UILabel!
     
-    @IBOutlet weak var mileageOutlet: UILabel!
-    
+
     @IBOutlet weak var locationOutlet: UILabel!
+    @IBOutlet weak var mileageOutlet: UILabel!
     
     @IBOutlet weak var phoneOutlet: UILabel!
     
-    @IBOutlet weak var imageOutlet: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-    @IBAction func CallDealer(_ sender: Any) {
-    }
     
+    @IBAction func CallDealer(_ sender: Any) {
+        if let url = URL(string: "tel://\(phoneOutlet.text!)"),
+        UIApplication.shared.canOpenURL(url) {
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
 }
+    
--- CarfaxInterview/ViewController.swift
+++ CarfaxInterview/ViewController.swift
@@ -36,6 +36,7 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
                         self.listings = myListings
                         DispatchQueue.main.sync {
                             print(self.listings)
+                            self.collectionView.reloadData()
                         }
                     }
                 }
@@ -45,17 +46,53 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return self.listings.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let row = listings[indexPath.row]
+        //print(row)
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "customCell", for: indexPath) as! CollectionViewCell
+        if let make = row["make"] as? String {
+            cell.makeOutlet.text = make
+        }
+        
+        if let model = row["model"] as? String {
+            cell.modelOutlet.text = model
+        }
+        if let year = row["year"] as? Int {
+            cell.yearOutlet.text = String(year)
+        }
+       
+        if let trim = row["trim"] as? String {
+            cell.trimOutlet.text = trim
+        }
+        
+        if let price = row["currentPrice"] as? Float {
+            cell.priceOutlet.text = "$" + String(price)
+        }
+        
+        if let mileage = row["mileage"] as? Float {
+            cell.mileageOutlet.text = String(mileage) + " Mi"
+        }
+
+        if let dealer = row["dealer"] as? Dictionary<String,Any> {
+            let phoneNumber = dealer["phone"] as? String
+            cell.phoneOutlet.text = phoneNumber
+            
+            let stateLocation = dealer["state"] as? String
+            let cityLocation = dealer["city"] as? String
+            //print(cityLocation)
+            cell.locationOutlet.text = (cityLocation ?? "") + ", " + (stateLocation ?? "")
+            
+        }
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let cellWidth = collectionView.frame.size.width
-        return CGSize(width: cellWidth, height: cellWidth*0.8)
+        let cellHeight = collectionView.frame.size.height
+        return CGSize(width: cellWidth, height: cellHeight * 0.4)
     }
 }
 
