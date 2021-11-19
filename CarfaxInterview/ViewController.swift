d45f7e96a623c58f5ced82a79d7340322f899104
Dhrumil <dhrumildesai181@gmail.com>
2021-11-19 09:04:17 -0500

    Fetch Data From Api

--- CarfaxInterview/ViewController.swift
+++ CarfaxInterview/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var listings: Array<Dictionary<String,Any>> = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let jsonString = "https://carfax-for-consumers.firebaseio.com/assignment.json"
+        guard let url = URL(string: jsonString) else { return }
+        
+        let session = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let httpResponse = response as? HTTPURLResponse {
+                if(httpResponse.statusCode != 200) {
+                    print("error")
+                }
+            }
+            
+            if let myData = data {
+                if let json = try? (JSONSerialization.jsonObject(with: myData, options: []) as! Dictionary<String,Any>) {
+                    if let myListings = json["listings"] as? Array<Dictionary<String,Any>> {
+                        self.listings = myListings
+                        DispatchQueue.main.sync {
+                            print(self.listings)
+                        }
+                    }
+                }
+            }
+        }
+        session.resume()
     }
-
-
 }
 
